1ba3cf3b9dcee8dff9c3e39a75a2af383da67db4
Artem18rus <urart88@mail.ru>
2023-01-15 16:49:26 +0400

    1

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,20 @@
+ const ShopItemFunc = ({item}) => {
+  return(
+    <div className="main-content">
+      <h2>Tiger of Sweden</h2>
+      <h1>Leonard coat</h1>
+      <h3>Minimalistic coat in cotton-blend</h3>
+      <div className="description">
+        Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.
+      </div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">£399.00</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  )
+}
+
+export default ShopItemFunc;
